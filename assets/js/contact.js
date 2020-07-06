8819f35842f88168e7afc3cd4ef3c625b3efce3e
jhonatasfender <jhonatas.fender@gmail.com>
2020-07-05 23:39:39 -0300

    implementando a mensagem de enviado com sucesso

--- assets/js/contact.js
+++ assets/js/contact.js
@@ -48,9 +48,28 @@ $(document).ready(function () {
       }),
     };
 
-    $.ajax(settings).done(function (response) {
-      console.log(response);
-    });
+    $.ajax(settings).done(function (response) { });
+
+    let button = $('.contact-form .theme-btn.btn-style-one, .contact-section .theme-btn.btn-style-one');
+    let buttonText = $('.contact-form .theme-btn.btn-style-one .btn-title, .contact-section .theme-btn.btn-style-one .btn-title');
+
+    const tickMark = `
+      <svg width="58" height="45" viewBox="0 0 58 45" xmlns="http://www.w3.org/2000/svg">
+        <path fill="#fff" fill-rule="nonzero" d="M19.11 44.64L.27 25.81l5.66-5.66 13.18 13.18L52.07.38l5.65 5.65"/>
+      </svg>
+    `;
+
+    buttonText.html(tickMark)
+    buttonText.addClass("button__circle").removeClass("btn-title")
+    button.addClass("btn-animation-custon-send-first")
+
+    setTimeout(() => {
+      buttonText.text("Clique para enviar");
+      buttonText.removeClass("button__circle").addClass("btn-title")
+      button.removeClass("btn-animation-custon-send-first")
+      $("#contact-form").get(0).reset()
+    }, 3000);
+
     return false
   });
 })
